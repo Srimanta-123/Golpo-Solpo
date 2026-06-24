dbbfde9a4d1f4bf5758b630840b4a90c6def53b2
Srimanta Chatterjee <srimantachatterjee02@gmail.com>
2026-06-25 01:55:45 +0530

    fix: correct case-sensitive import path for Signup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import Home from './pages/home/Home.jsx'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './pages/authentication/Login.jsx'
-import Signup from './Pages/authentication/Signup.jsx'
+import Signup from './pages/authentication/Signup.jsx'
 
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
